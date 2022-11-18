76a98ce91d7be6607731d2f162537c023a3d9dcb
Rodrigo Castilho <rcastilho@gmail.com>
2022-11-17 22:50:59 -0300

    css

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import FormLogin from '../../components/FormLogin/FormLogin'
 import styles from './Login.module.css'
+import {FaUserAstronaut} from 'react-icons/fa'
 
 const Login = () => {
   return (
     <div className={`${styles.container}`}>
+      <div className={`${styles.avatar}`}>
+        <FaUserAstronaut size={80} className={`${styles.icon}`}/>
+      </div>
         <FormLogin/>
     </div>
   )
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import styles from '../Login/Login.module.css'
 import FormRegister from './../../components/FormRegister/FormRegister';
+import {FiDatabase} from 'react-icons/fi'
 
 const Register = () => {
   return (
     <div className={`${styles.container}`}>
+      <div className={`${styles.avatar}`}>
+      <FiDatabase size={60} className={`${styles.icon}`}/>
+
+      </div>
         <FormRegister/>
     </div>
   )
