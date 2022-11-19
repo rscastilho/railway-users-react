86adba88b747a2426d0962a9e7a143bae9f64fb8
Rodrigo Castilho <rcastilho@gmail.com>
2022-11-18 22:08:05 -0300

    login001

--- src/components/FormLogin/FormLogin.js
+++ src/components/FormLogin/FormLogin.js
@@ -27,11 +27,11 @@ const FormLogin = () => {
         try {
             const result = await LoginApi({ email, password })
             if (result.token) {
-                // localStorage.clear()
-                // localStorage.setItem('@id', result.data[0].id)
-                // localStorage.setItem('@name', result.data[0].name)
-                // localStorage.setItem('@email', result.data[0].email)
-                // localStorage.setItem('@token', result.token)
+                localStorage.clear()
+                localStorage.setItem('@id', result.data[0].id)
+                localStorage.setItem('@name', result.data[0].name)
+                localStorage.setItem('@email', result.data[0].email)
+                localStorage.setItem('@token', result.token)
                 navigate('/home')
                 Toast.fire({
                     icon: 'success',
