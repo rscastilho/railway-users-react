5f3357b489b81f6a171faae1fa397ea27c7248ff
Rodrigo Castilho <rcastilho@gmail.com>
2022-11-18 22:10:26 -0300

    consoleapi

--- src/api/LoginApi.js
+++ src/api/LoginApi.js
@@ -3,6 +3,7 @@ import webApi from "./webApi"
 const LoginApi = async (dataLogin) => {
     try {
         const result = await webApi.post('/login', dataLogin)
+        console.log('resultado api',result)
         return result.data
     } catch (error) {
         return error.response.data
